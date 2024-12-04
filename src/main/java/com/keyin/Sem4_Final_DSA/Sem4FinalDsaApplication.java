2b31b53608a66d01a15bbcbfb951ddd6cc62803a
espurrell <150044207+espurrell@users.noreply.github.com>
2024-12-04 17:51:31 -0330

    Init Commit

---
+++ src/main/java/com/keyin/Sem4_Final_DSA/Sem4FinalDsaApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.Sem4_Final_DSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem4FinalDsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem4FinalDsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyin/Sem4_Final_DSA/Sem4FinalDsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.Sem4_Final_DSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem4FinalDsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
